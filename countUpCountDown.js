3942df99ac7ddcf56d0287f8d949bb7cf3b63aa1
irynanetrebska <irina.netrebska@gmail.com>
2023-05-01 15:42:05 +0200

    Used Recursion to Create a Range of Numbers

--- countUpCountDown.js
+++ countUpCountDown.js
@@ -26,3 +26,16 @@ console.log(countup(5));
 function countdown(n) {
   return n < 1 ? [] : [n].concat(countdown(n - 1));
 }
+
+// Use Recursion to Create a Range of Numbers
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum > endNum) {
+    return [];
+  } else {
+    const myArray = rangeOfNumbers(startNum + 1, endNum);
+    myArray.unshift(startNum);
+    return myArray;
+  }
+}
+
+console.log(rangeOfNumbers(6, 9));
